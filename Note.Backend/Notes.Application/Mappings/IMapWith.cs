67f36c7c9eb16a3dfc48e8679a52d08fee7da633
Proces <pocerfeus@gmail.com>
2023-10-23 20:16:52 +0100

    Added all files

--- Note.Backend/Notes.Application/Mappings/IMapWith.cs
+++ Note.Backend/Notes.Application/Mappings/IMapWith.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Notes.Application.Mappings
 {
-    internal interface IMapWith
+    public interface IMapWith<T>
     {
+        public void Mapping (Profile profile)=> profile.CreateMap(typeof(T), GetType());
     }
 }
